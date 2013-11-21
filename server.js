06217a951c483643448af9f0898037841f6ec670
Paul Panserrieu <paul.panserrieu@gmail.com>
2013-11-21 10:46:33 +0100

    ...

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var port = process.env.PORT || 8001;
 var http = require('http');
 var server = http.createServer(function (request, response) {
         response.writeHead(200, {"Content-Type": "text/plain"});
-        response.end("watchnpm\n");
+        response.end("watchnpm...\n");
      });
 
 server.listen(port);
